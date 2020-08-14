2f7b210b8ab37aee8e334fb06ccdb7e0256fd0ed
Adrian Espinoza <aespinoza@devsu.com>
2020-08-13 19:25:18 -0500

    update

--- src/app/scheduler/services/reminders-seeder.ts
+++ src/app/scheduler/services/reminders-seeder.ts
@@ -8,7 +8,8 @@ const getSeedData = () => [
   new Reminder(
     "She cried diamonds",
     moment(
-      `${now.getMonth()}-2-${now.getFullYear()} 13:20", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-2-${now.getFullYear()} 13:20`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Guayaquil",
     DEFAULT_COLORS[0].value
@@ -16,7 +17,8 @@ const getSeedData = () => [
   new Reminder(
     "Dont step on the broken glass",
     moment(
-      `${now.getMonth()}-3-${now.getFullYear()} 14:15", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-3-${now.getFullYear()} 14:15`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Quito",
     DEFAULT_COLORS[2].value
@@ -24,7 +26,8 @@ const getSeedData = () => [
   new Reminder(
     "Deep dive down the rabbit hole",
     moment(
-      `${now.getMonth()}-4-${now.getFullYear()} 08:15", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-4-${now.getFullYear()} 08:15`,
+      "MM-DD-YYYY HH:mm"
     ),
     "London",
     DEFAULT_COLORS[1].value
@@ -32,7 +35,8 @@ const getSeedData = () => [
   new Reminder(
     "Final Fantasy Vii",
     moment(
-      `${now.getMonth()}-6-${now.getFullYear()} 10:12", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-6-${now.getFullYear()} 10:12`,
+      "MM-DD-YYYY HH:mm"
     ),
     "New York",
     DEFAULT_COLORS[0].value
@@ -40,7 +44,8 @@ const getSeedData = () => [
   new Reminder(
     "Kingdom Hearts III",
     moment(
-      `${now.getMonth()}-6-${now.getFullYear()} 12:08", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-6-${now.getFullYear()} 12:08`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Miami",
     DEFAULT_COLORS[0].value
@@ -48,7 +53,8 @@ const getSeedData = () => [
   new Reminder(
     "Super Mario",
     moment(
-      `${now.getMonth()}-10-${now.getFullYear()} 15:45", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-10-${now.getFullYear()} 15:45`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Cuenca",
     DEFAULT_COLORS[0].value
@@ -56,7 +62,8 @@ const getSeedData = () => [
   new Reminder(
     "Dissidia Final Fantasy",
     moment(
-      `${now.getMonth()}-13-${now.getFullYear()} 16:19", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-13-${now.getFullYear()} 16:19`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Moscow",
     DEFAULT_COLORS[2].value
@@ -64,7 +71,8 @@ const getSeedData = () => [
   new Reminder(
     "League of Legends",
     moment(
-      `${now.getMonth()}-14-${now.getFullYear()} 08:16", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-14-${now.getFullYear()} 08:16`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Istanbul",
     DEFAULT_COLORS[0].value
@@ -72,7 +80,8 @@ const getSeedData = () => [
   new Reminder(
     "Monster Hunter",
     moment(
-      `${now.getMonth()}-14-${now.getFullYear()} 09:32", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-14-${now.getFullYear()} 09:32`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Bucharest",
     DEFAULT_COLORS[5].value
@@ -80,7 +89,8 @@ const getSeedData = () => [
   new Reminder(
     "Chrono trigger",
     moment(
-      `${now.getMonth()}-14-${now.getFullYear()} 12:05", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-14-${now.getFullYear()} 12:05`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Dalian",
     DEFAULT_COLORS[1].value
@@ -88,7 +98,8 @@ const getSeedData = () => [
   new Reminder(
     "Resident Evil",
     moment(
-      `${now.getMonth()}-14-${now.getFullYear()} 16:04", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-14-${now.getFullYear()} 16:04`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Hamburg",
     DEFAULT_COLORS[4].value
@@ -96,7 +107,8 @@ const getSeedData = () => [
   new Reminder(
     "Dota",
     moment(
-      `${now.getMonth()}-14-${now.getFullYear()} 23:22", "MM-DD-YYYY HH:mm`
+      `${now.getMonth()}-14-${now.getFullYear()} 23:22`,
+      "MM-DD-YYYY HH:mm"
     ),
     "Shantou",
     DEFAULT_COLORS[3].value
